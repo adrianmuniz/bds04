2467e3bff7fcb15d48aee12575d14a35947d8523
Cainã Adrian Muniz <adrianmuniz.ceo@outlook.com>
2022-11-16 14:04:33 -0300

    CRUD

---
+++ src/main/java/com/devsuperior/bds04/service/exceptions/DataBaseException.java
@@ -0,0 +1,11 @@
+package com.devsuperior.bds04.service.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+
+}
